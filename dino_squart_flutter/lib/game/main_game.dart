903b66faf77878a12c45e42cb397df1a4b40b69a
JunH1004 <junejune040218@naver.com>
2023-11-17 15:22:36 +0900

    스쿼트하면 점프!

--- dino_squart_flutter/lib/game/main_game.dart
+++ dino_squart_flutter/lib/game/main_game.dart
@@ -21,6 +21,9 @@ class MainGame extends FlameGame with HasCollisionDetection{
   late Cloud cloud;
   EnemyManager enemyManager = EnemyManager();
   GameManager gameManager = GameManager();
+
+
+  int lastSquatCnt = 0;
   BuildContext context;
   @override
   Color backgroundColor() {
@@ -49,13 +52,11 @@ class MainGame extends FlameGame with HasCollisionDetection{
 
   void update(double dt) {
     super.update(dt);
-
-    // WorkoutInfo 클래스를 가져옴
-    WorkoutInfo workoutInfo = Provider.of<WorkoutInfo>(context, listen: false);
-
-    // 스쿼트 개수에 따라 캐릭터의 y 위치 조절
-    double newYPosition = size.y - 20 - workoutInfo.squatCount * 20; // 스쿼트 개수에 따라 조절
-    //player.position = Vector2(size.x * 0.25, newYPosition);
+    int squatCnt = context.read<WorkoutInfo>().squatCount;
+    if (lastSquatCnt != squatCnt){
+      lastSquatCnt = squatCnt;
+      player.jump();
+    }
   }
 
 }
